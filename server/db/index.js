9bae1f47de1fe6b42bfb7eea85dd8d8334a6ce94
joeyholland15 <joeyholland15@gmail.com>
2015-12-18 20:45:55 -0800

    Forgot to save one file on last commit.

--- server/db/index.js
+++ server/db/index.js
@@ -4,12 +4,9 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-module.exports.dbConnection = mysql.createConnection({
+module.exports = mysql.createConnection({
     user: "root",
     password: "tessaandjoey",
     database: "chat"
   });
- module.exports.connection = function(){
-   return dbConnection.connect();
- };
 
