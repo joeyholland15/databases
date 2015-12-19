b21fd9f452605d26ea6561d6906e30d4dd34bf81
joeyholland15 <joeyholland15@gmail.com>
2015-12-18 17:24:26 -0800

    Added client code and created messages table.

--- server/schema.sql
+++ server/schema.sql
@@ -4,6 +4,15 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL,
+  message_text VARCHAR(100),
+  created_at TIME(0),
+  room_id INT,
+  user_id INT,
+  PRIMARY KEY(id)
+  /* we have not created ref table yet */
+  -- FOREIGN KEY (room_id) REFERENCES rooms (id),
+  -- FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
 /* Create other tables and define schemas for them here! */
